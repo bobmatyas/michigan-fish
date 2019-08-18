69cf0c1bcc25b3c4045dcf2c6b81195ab606e264
bobmatyas <ballium@protonmail.com>
2019-08-17 21:10:57 -0500

    Updates

---
+++ precache-manifest.1f635962417c0d0da775ccb568766143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0874b5f1d490856edfbe9d5c9b3726b4",
+    "url": "/michigan-fish/index.html"
+  },
+  {
+    "revision": "ffeb7b3924aaf09e59dc",
+    "url": "/michigan-fish/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3b1ec16320d85334f0d8",
+    "url": "/michigan-fish/static/js/2.e50a068a.chunk.js"
+  },
+  {
+    "revision": "ffeb7b3924aaf09e59dc",
+    "url": "/michigan-fish/static/js/main.0b4e0e76.chunk.js"
+  },
+  {
+    "revision": "95992046f9605821a33a",
+    "url": "/michigan-fish/static/js/runtime~main.10b9e8c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/michigan-fish/static/media/logo.5d5d9eef.svg"
+  }
+]);
